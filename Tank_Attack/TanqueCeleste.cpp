b82a7750d78e95ee569226bbebd3272126398461
Izaguirre1g <jo.garcia@estudiantec.cr>
2024-09-29 19:23:00 -0600

    TanqueCeleste (Pablo)

---
+++ Tank_Attack/TanqueCeleste.cpp
@@ -0,0 +1,21 @@
+#include "TanqueCeleste.h"
+#include "BFS.h"
+#include <cstdlib> // Para rand()
+
+TanqueCeleste::TanqueCeleste(Grafo* grafo, int nodoInicial) : Tanque(grafo, nodoInicial) {}
+
+void TanqueCeleste::mover() {
+    if (rand() % 2 == 0) {
+        std::cout << "Movimiento aleatorio de celeste" << std::endl;
+        // Movimiento aleatorio
+        int numNodos = grafo->getNumNodos();
+        nodoActual = rand() % numNodos;
+    } else {
+        std::cout << "Movimiento mediante BFS de celeste" << std::endl;
+        // Movimiento mediante BFS
+        if (nodoObjetivo != -1) {
+            bfs(*grafo, nodoActual, nodoObjetivo);
+            nodoActual = nodoObjetivo; // Para simplificar, asumimos que el tanque llega al objetivo
+        }
+    }
+}
---
+++ Tank_Attack/TanqueCeleste.h
@@ -0,0 +1,13 @@
+#ifndef TANQUECELESTE_H
+#define TANQUECELESTE_H
+
+#include "Tanque.h"
+
+class TanqueCeleste : public Tanque {
+public:
+    TanqueCeleste(Grafo* grafo, int nodoInicial);
+
+    void mover() override;
+};
+
+#endif // TANQUECELESTE_H
